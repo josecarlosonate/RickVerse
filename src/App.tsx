e51febd3f471469266d2598a87be169e1d673901
Jose Carlos Onate Rodriguez <ingeniero.josec@gmail.com>
2026-09-24 13:14:45 -0500

    feat: implement multi-filter system and custom alerts

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import './App.css'
 import CharacterCards from './components/CharacterCards'
 import CharacterSkeleton from './components/CharacterSkeleton'
 import Header from './components/Header'
-import Search from './components/Search'
+import Filters from './components/Filters'
 import { useCharacter } from './hooks/useCharacter'
+import Alert from './components/Alert'
 
 function App() {
 
-  const { characters, isLoading } = useCharacter()
+  const { characters, isLoading, error, search, setSearch, status, setStatus, gender, setGender } = useCharacter()
 
   return (
     <>
       <Header />
-      <Search />
-      {isLoading ? <CharacterSkeleton /> : <CharacterCards characters={characters} />}
+      <Filters value={search} onChange={setSearch} statusValue={status} onStatusChange={setStatus} genderValue={gender} onGenderChange={setGender} />
+      {
+        error ? <Alert type='error' message={error} /> :
+          isLoading ? <CharacterSkeleton /> :
+            characters.length === 0 ? <Alert type='info' message='No characters found' /> :
+              <CharacterCards characters={characters} />
+      }
     </>
   )
 }
--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -6,24 +6,46 @@ import { getCharacters } from "../api/rickAndMorty";
 export function useCharacter() {
     const [characters, setCharacters] = useState<character[]>([])
     const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+    const [search, setSearch] = useState<string>('')
+    const [debouncedSearch, setDebouncedSearch] = useState('')
+    const [status, setStatus] = useState('all')
+    const [gender, setGender] = useState("")
 
     useEffect(() => {
 
+        setError(null)
+        setIsLoading(true)
+
         async function loadCharacter() {
             try {
-                const results = await getCharacters()
+                const results = await getCharacters({ name: debouncedSearch, status, gender })
                 setCharacters(results)
                 setIsLoading(false)
             } catch (error) {
+                setError('Failed to fetch characters')
                 setIsLoading(false)
             }
         }
 
         loadCharacter()
-    }, [])
+    }, [debouncedSearch, status, gender])
+
+    // EFECTO 2: Maneja el temporizador para retrasar la búsqueda (Antirrebote / Debounce)
+    useEffect(() => {
+        const id = setTimeout(() => setDebouncedSearch(search), 500)
+        return () => clearTimeout(id);
+    }, [search])
 
     return {
         characters,
-        isLoading
+        isLoading,
+        error,
+        search,
+        setSearch,
+        status,
+        setStatus,
+        gender,
+        setGender
     }
 }
